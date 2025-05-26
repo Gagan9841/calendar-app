2f25e63aaa5009fdac493a48845d6acbe409bbaa
Gagan Acharya <gagan.acharya>
2025-05-26 10:31:44 +0545

    feat: initialize Tauri application with basic configuration and setup
    - Added Cargo.toml for Rust dependencies and package configuration.
    - Created build.rs to handle Tauri build process.
    - Defined default capabilities in default.json.
    - Added various icon files for application branding.
    - Implemented basic Rust command in lib.rs to greet users.
    - Set up main.rs to run the Tauri application.
    - Configured tauri.conf.json with application settings and build commands.
    - Updated TypeScript configuration files for improved type checking and module resolution.

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,67 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import Components from 'unplugin-vue-components/vite'
+
+const host = process.env.TAURI_DEV_HOST
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    tailwindcss(),
+    Components({
+      dts: true,
+      extensions: ['vue'],
+      globs: ['src/components/**/*.{vue}', 'src/components/'],
+      deep: true,
+      types: [
+        {
+          from: 'vue-router',
+          names: ['RouterLink', 'RouterView'],
+        },
+        {
+          from: 'vue',
+          names: [
+            'defineComponent',
+            'ref',
+            'reactive',
+            'computed',
+            'watch',
+            'onMounted',
+            'onUnmounted',
+            'onBeforeMount',
+            'onBeforeUnmount',
+          ],
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: 'ws',
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ['**/src-tauri/**'],
     },
   },
 })
